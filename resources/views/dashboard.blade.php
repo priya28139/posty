3132ed8bc6669764e42ad4f32abe0d78d90bd0b6
priya28139 <priya.aswani28139@gmail.com>
2021-03-06 23:30:12 +0400

    Create dashboard view using dashboard.blade.php

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="flex justify-center">
+    <div class="w-full bg-white p-6 rounded-lg">
+        <h2 class="text-1xl text-gray-900">
+            Dashboard
+        </h2>
+    </div>
+</div>
+@endsection
