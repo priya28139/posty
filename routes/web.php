fc21efadaf64ad4869c60cef7a966589da3fe6df
priya28139 <priya.aswani28139@gmail.com>
2021-03-11 18:12:36 +0400

    Set temporary auth middleware for `posts` route in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', function () {
     return view('posts.index');
-});
+})->name('posts')->middleware('auth');
