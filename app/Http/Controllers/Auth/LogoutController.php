340fad6159ef7f117a3e9a45717a125ed3905c5d
priya28139 <priya.aswani28139@gmail.com>
2021-03-11 18:10:50 +0400

    Add auth middleware to Logout

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function store()
     {
         Auth::logout();
