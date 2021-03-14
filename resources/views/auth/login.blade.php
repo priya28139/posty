912a0066961bd4f92ab77f5fe94caa085a798292
priya28139 <priya.aswani28139@gmail.com>
2021-03-14 10:32:05 +0400

    Remove hidden input for `remember me` from Login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,6 @@
         </div>
         <form class="mt-8 space-y-6" action="{{ route('login') }}" method="POST">
             @csrf
-            <input type="hidden" name="remember" value="true">
             <div class="rounded-md shadow-sm -space-y-px">
 
                 <!-- email -->
